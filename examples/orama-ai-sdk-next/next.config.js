c6c5c565e6b5bf77da88a7b5fc69be98650147dd
Aileen Villanueva <aileenvl@gmail.com>
2024-12-23 11:46:35 -0600

    fix: update scope in next.config

--- examples/orama-ai-sdk-next/next.config.js
+++ examples/orama-ai-sdk-next/next.config.js
@@ -4,7 +4,14 @@ const nextConfig = {
     ORAMA_API_KEY: process.env.ORAMA_API_KEY,
     ORAMA_API_URL: process.env.ORAMA_API_URL,
   },
-  transpilePackages: ['@orama/ai-sdk-provider'],
+  transpilePackages: ['@oramacloud/ai-sdk-provider'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@oramacloud/ai-sdk-provider': require.resolve('@oramacloud/ai-sdk-provider')
+    }
+    return config
+  },
   images: {
     domains: ['website-assets.oramasearch.com',
       'pub-63f27474fef34587a26f8ec7f626984d.r2.dev'],
