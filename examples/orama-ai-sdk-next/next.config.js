26028785efaf232812c3c174bca1e3243c6a12c1
Aileen Villanueva <aileenvl@gmail.com>
2024-12-23 11:48:50 -0600

    fix: remove webpack config

--- examples/orama-ai-sdk-next/next.config.js
+++ examples/orama-ai-sdk-next/next.config.js
@@ -5,13 +5,6 @@ const nextConfig = {
     ORAMA_API_URL: process.env.ORAMA_API_URL,
   },
   transpilePackages: ['@oramacloud/ai-sdk-provider'],
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@oramacloud/ai-sdk-provider': require.resolve('@oramacloud/ai-sdk-provider')
-    }
-    return config
-  },
   images: {
     domains: ['website-assets.oramasearch.com',
       'pub-63f27474fef34587a26f8ec7f626984d.r2.dev'],
